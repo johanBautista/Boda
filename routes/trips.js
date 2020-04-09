c537951615d9372010d92360a4375bf8c45f3812
johan Bautista <jsbparra@gmail.com>
2020-04-09 18:09:09 +0200

    eliminar vistas no usadas

--- routes/trips.js
+++ routes/trips.js
@@ -1,34 +1,34 @@
-var express = require('express');
-var router = express.Router();
-const Trip = require('../models/trip');
+// var express = require('express');
+// var router = express.Router();
+// const Trip = require('../models/trip');
 
-const User = require('../models/user');
+// const User = require('../models/user');
 
-/* GET map page. */
-router.get('/', function(req, res, next) {
-  res.render('trips/trips', { title: 'Chofer Map' });
-});
+// /* GET map page. */
+// router.get('/', function(req, res, next) {
+//   res.render('trips/trips', { title: 'Chofer Map' });
+// });
 
-// Post to save trip to db
-router.post('/', function(req,res,next){
+// // Post to save trip to db
+// router.post('/', function(req,res,next){
   
-  const username = req.session.currentUser.email;
-  var origin = req.body.origin;
-  var destination = req.body.destination;
-  var service = req.body.service;
-  var cost = req.body.cost;
-  var time = req.body.time;
-  var distance = req.body.distance;
-  var name = req.body.name;
+//   const username = req.session.currentUser.email;
+//   var origin = req.body.origin;
+//   var destination = req.body.destination;
+//   var service = req.body.service;
+//   var cost = req.body.cost;
+//   var time = req.body.time;
+//   var distance = req.body.distance;
+//   var name = req.body.name;
   
 
-  Trip.create({ username, origin, destination, service, time, distance, cost,name})
-    .then(data => {
-      res.redirect('/routes');
-    })
-    .catch(error => {
-      next(error);
-    });
-});
+//   Trip.create({ username, origin, destination, service, time, distance, cost,name})
+//     .then(data => {
+//       res.redirect('/routes');
+//     })
+//     .catch(error => {
+//       next(error);
+//     });
+// });
 
-module.exports = router;
+// module.exports = router;
