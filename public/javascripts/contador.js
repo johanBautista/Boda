b5cc9edfb4887f50fb34ce6f11b9d0377698dc33
johan Bautista <jsbparra@gmail.com>
2020-04-09 15:45:33 +0200

    omitir main.js y cambiar codigo contador

--- public/javascripts/contador.js
+++ public/javascripts/contador.js
@@ -1,21 +1,40 @@
+// const second = 1000,
+//   minute = second * 60,
+//   hour = minute * 60,
+//   day = hour * 24;
+
+// let countDown = new Date('Aug 1, 2020 17:00:00').getTime(),
+//   x = setInterval(function () {
+//     let now = new Date().getTime(),
+//       distance = countDown - now;
+
+//     (document.getElementById('days').innerText = Math.floor(distance / day)),
+//       (document.getElementById('hours').innerText = Math.floor(
+//         (distance % day) / hour,
+//       )),
+//       (document.getElementById('minutes').innerText = Math.floor(
+//         (distance % hour) / minute,
+//       )),
+//       (document.getElementById('seconds').innerText = Math.floor(
+//         (distance % minute) / second,
+//       ));
+//   }, second);
 const second = 1000,
-  minute = second * 60,
-  hour = minute * 60,
-  day = hour * 24;
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
 
 let countDown = new Date('Aug 1, 2020 17:00:00').getTime(),
-  x = setInterval(function () {
-    let now = new Date().getTime(),
-      distance = countDown - now;
+    x = setInterval(function() {    
 
-    (document.getElementById('days').innerText = Math.floor(distance / day)),
-      (document.getElementById('hours').innerText = Math.floor(
-        (distance % day) / hour,
-      )),
-      (document.getElementById('minutes').innerText = Math.floor(
-        (distance % hour) / minute,
-      )),
-      (document.getElementById('seconds').innerText = Math.floor(
-        (distance % minute) / second,
-      ));
-  }, second);
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+     
+
+    }, second)
