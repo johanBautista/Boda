504a52f807328ba8934179d2296da830141b97b8
johan Bautista <jsbparra@gmail.com>
2020-04-09 15:39:18 +0200

    omitir modelos-routes,tarifas,trip

--- models/trip.js
+++ models/trip.js
@@ -1,23 +1,23 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-const tripSchema = new Schema({
-     username: String,
-     origin: String,
-     destination: String,
-     service: String,
-     distance: Number,
-     time: Number,
-     cost: Number,
-     name:String
-}, {
-    timestamps: {
-      createdAt: "created_at",
-      updatedAt: "updated_at"
-    }
-  });
+// const tripSchema = new Schema({
+//      username: String,
+//      origin: String,
+//      destination: String,
+//      service: String,
+//      distance: Number,
+//      time: Number,
+//      cost: Number,
+//      name:String
+// }, {
+//     timestamps: {
+//       createdAt: "created_at",
+//       updatedAt: "updated_at"
+//     }
+//   });
 
-const Trip = mongoose.model("Trip", tripSchema);
+// const Trip = mongoose.model("Trip", tripSchema);
 
-module.exports = Trip;
+// module.exports = Trip;
 
